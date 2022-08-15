bc760bc245f5cab4c425af1bfba52a02ec6a6ebf
Zzzul <807fahmi@gmail.com>
2022-08-15 15:05:09 +0700

    fix image broken at login & register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,9 +12,10 @@
             <div id="auth-left">
                 <div class="auth-logo" >
                     <a href="index.html">
-                        <img src="{{ asset('mazer') }}/images/logo/logo.png" alt="Logo">
+                        <img src="{{ asset('mazer') }}/images/logo/logo.svg" alt="Logo">
                     </a>
                 </div>
+
                 <h1 class="auth-title">{{ __('Log in.') }}</h1>
                 <p class="auth-subtitle mb-3">{{ __('Log in with your data that you entered during registration.') }}</p>
 
