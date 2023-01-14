5de0225777fe9495707e3b28eaf37618f4cdd7cd
Zzzul <807fahmi@gmail.com>
2023-01-14 17:06:46 +0700

    fix role and permission seeder(#30)

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -21,7 +21,7 @@ class RoleAndPermissionSeeder extends Seeder
 
         foreach (config('permission.permissions') as $permission) {
             foreach ($permission['access'] as $access) {
-                Permission::create(['name' => $access]);
+                Permission::firstOrCreate(['name' => $access]);
             }
         }
 
